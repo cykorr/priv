34895c28d706b5eab35c111935a9946011ed3790
xkorrupt <134173277+xkorrupt@users.noreply.github.com>
2024-01-27 22:51:20 +0000

    Add files via upload

---
+++ gitfiti.ps1
@@ -0,0 +1,421 @@
+cd $PSScriptRoot
+$REPO="priv"
+git init $REPO
+cd $REPO
+New-Item README.md -ItemType file | Out-Null
+git add README.md
+New-Item gitfiti -ItemType file | Out-Null
+git add gitfiti
+$Env:GIT_AUTHOR_DATE="2023-02-05T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-05T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-05T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-05T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-05T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-05T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-05T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-05T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-06T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-06T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-06T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-06T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-06T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-06T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-06T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-06T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-07T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-07T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-07T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-07T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-07T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-07T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-07T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-07T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-08T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-08T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-08T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-08T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-08T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-08T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-08T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-08T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-09T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-09T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-09T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-09T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-09T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-09T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-09T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-09T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-11T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-11T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-11T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-11T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-11T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-11T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-11T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-11T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-12T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-12T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-12T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-12T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-12T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-12T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-12T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-12T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-13T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-13T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-13T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-13T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-13T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-13T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-14T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-14T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-15T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-15T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-15T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-15T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-15T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-15T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-16T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-16T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-16T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-16T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-16T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-16T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-16T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-16T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-18T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-18T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-18T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-18T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-18T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-18T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-18T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-18T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-19T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-19T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-19T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-19T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-19T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-19T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-19T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-19T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-20T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-20T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-20T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-20T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-20T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-20T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-21T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-21T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-21T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-21T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-21T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-21T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-22T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-22T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-22T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-22T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-22T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-22T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-23T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-23T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-23T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-23T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-23T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-23T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-23T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-23T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-24T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-24T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-24T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-24T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-24T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-24T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-24T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-24T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-25T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-25T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-25T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-25T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-25T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-25T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-25T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-25T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-26T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-26T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-26T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-26T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-26T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-26T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-26T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-26T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-27T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-27T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-27T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-27T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-27T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-27T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-28T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-28T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-28T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-28T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-02-28T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-02-28T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-01T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-01T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-01T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-01T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-01T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-01T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-02T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-02T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-02T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-02T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-02T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-02T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-02T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-02T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-03T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-03T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-03T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-03T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-03T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-03T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-03T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-03T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-04T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-04T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-04T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-04T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-04T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-04T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-04T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-04T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-05T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-05T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-05T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-05T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-05T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-05T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-05T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-05T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-06T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-06T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-06T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-06T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-06T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-06T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-07T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-07T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-08T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-08T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-08T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-08T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-08T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-08T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-09T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-09T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-09T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-09T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-09T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-09T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-09T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-09T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-11T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-11T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-11T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-11T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-11T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-11T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-11T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-11T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-12T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-12T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-12T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-12T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-12T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-12T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-12T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-12T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-13T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-13T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-13T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-13T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-13T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-13T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-13T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-13T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-14T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-14T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-14T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-14T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-14T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-14T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-14T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-14T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-15T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-15T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-15T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-15T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-15T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-15T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-15T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-15T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-16T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-16T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-16T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-16T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-16T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-16T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-16T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-16T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-18T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-18T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-18T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-18T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-18T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-18T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+$Env:GIT_AUTHOR_DATE="2023-03-18T12:00:00"
+$Env:GIT_COMMITTER_DATE="2023-03-18T12:00:00"
+git commit --allow-empty -m "gitfiti" | Out-Null
+
+git branch -M main
+git remote add origin git@github.com:xkorrupt/$REPO.git
+git pull origin main
+git push -u origin main
